a70f588f6e2cd0fc049624df1167c6b70ef66a5c
pensin18 <75penchala@gmail.com>
2025-12-21 18:28:48 +0530

    21-12/25

---
+++ 13-12/25/revision.py
@@ -0,0 +1,12 @@
+students = [
+    {'name':'Alice', 'age': 23},
+    {'name':'Bob','age':25},
+    {'name':'charlie','age':22}
+]
+
+course = {
+    'name':'Python Programming',
+    'duration':'3 months',
+    'topics':['Basics', 'OOP', 'Data Science']
+    }
+
